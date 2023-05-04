26b70f72d930d4a2a6239dd2bfe43f00693753dd
Eelaya Darsini <eelayadarsini@gmail.com>
2023-05-04 15:03:14 +0800

    settings updated

--- setting.js
+++ setting.js
@@ -57,6 +57,26 @@ for (var i = 0; i < themeRadios.length; i++) {
   themeRadios[i].addEventListener('change', handleThemeChange);
 }
 
+body = document.querySelector("body");
+toggle = document.querySelector(".toggle");
+
+getMode = localStorage.getItem("mode");
+if(getMode && getMode==="dark"){
+  body.classList.add("dark");
+  toggle.classList.add("active");
+}
+
+toggle.addEventListener("click", () => {
+  body.classList.toggle("dark");
+
+  if(!body.classList.contains("dark")){
+    return localStorage.setItem("mode", "light");
+  }
+  localStorage.setItem("mode", "dark");
+});
+
+toggle.addEventListener("click", () => toggle.classList.toggle("active"));
+
 //rating bar
 rating = document.getElementsByName('rating');
 
